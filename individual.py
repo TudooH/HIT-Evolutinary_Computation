d6abebe704c5525c104cee34118d827037125c55
TudooH <1014090526@qq.com>
2020-12-15 19:17:00 +0800

    update individual

--- individual.py
+++ individual.py
@@ -6,16 +6,19 @@ class Individual:
     def __init__(self, tsp):
         self.__name, self.__comment, self.__tsp_type, self.__num, self.__edge_type, self.__nodes = tsp.get_info()
 
-    def get_route(self):
         route = [i for i in range(self.__num)]
         random.shuffle(route)
-        route.append(route[0])
-        return route
+        self.__route = route
 
-    def get_dis(self, route):
+    def get_route(self):
+        return self.__route[:]
+
+    def get_dis(self):
+        route = self.__route[:]
+        route.append(route[0])
         dis = 0
         for i in range(self.__num):
             x1, y1 = self.__nodes[route[i]][0], self.__nodes[route[i]][1]
             x2, y2 = self.__nodes[route[i + 1]][0], self.__nodes[route[i + 1]][1]
             dis += math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
-        return dis, route
+        return dis
--- population.py
+++ population.py
@@ -1,11 +1,4 @@
-from abc import ABCMeta, abstractmethod
 
 
-class Population(metaclass=ABCMeta):
-    @abstractmethod
-    def get_route(self):
-        pass
-
-    @abstractmethod
-    def get_dis(self, route):
-        pass
+class Population():
+    pass
